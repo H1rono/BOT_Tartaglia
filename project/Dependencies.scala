6f435588af0d2e3fa7fe9af4337503aea303c7ca
H1rono <hronok66@gmail.com>
2024-02-24 23:04:18 +0900

    Add http4s-client in dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Dependencies {
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
   private val http4sVersion = "0.23.25"
   lazy val http4s = Seq(
+    "org.http4s" %% "http4s-ember-client" % http4sVersion,
     "org.http4s" %% "http4s-ember-server" % http4sVersion,
     "org.http4s" %% "http4s-dsl" % http4sVersion,
     "org.http4s" %% "http4s-circe" % http4sVersion
