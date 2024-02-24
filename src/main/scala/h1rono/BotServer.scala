b75858f01bf7f7737dff59334eece820db417385
H1rono <hronok66@gmail.com>
2024-02-24 22:36:05 +0900

    Refactor

--- src/main/scala/h1rono/BotServer.scala
+++ src/main/scala/h1rono/BotServer.scala
@@ -5,39 +5,43 @@ import cats.syntax.all._
 import com.comcast.ip4s._
 import fs2.io.net.Network
 import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Server
 import org.http4s.implicits._
 import cats.effect.std.Console
+import cats.data.EitherT
+import cats.effect.kernel.Resource
+import org.http4s.HttpApp
+import cats.effect.std.Env
 
 object BotServer {
-  def run[F[_]: Async: Network: Console]: F[Nothing] = {
-    val verificationToken = loadEnv("VERIFICATION_TOKEN")
-    val accessToken = loadEnv("BOT_ACCESS_TOKEN")
-
-    val helloWorldAlg = HelloWorld.impl[F]
-    val dumpReqAlg = DumpReq.impl[F]
-    val botHandlerAlg = BotHandler.impl[F](BotHandler.Config(verificationToken, accessToken))
+  def run[F[_]: Async: Network: Console: Env]: EitherT[F, String, Resource[F, Server]] = for {
+    verificationToken <- loadEnvF[F]("VERIFICATION_TOKEN")
+    accessToken <- loadEnvF[F]("BOT_ACCESS_TOKEN")
+    helloWorldAlg = HelloWorld.impl[F]
+    dumpReqAlg = DumpReq.impl[F]
+    botHandlerAlg = BotHandler.impl[F](BotHandler.Config(verificationToken, accessToken))
 
     // Combine Service Routes into an HttpApp.
     // Can also be done via a Router if you
     // want to extract segments not checked
     // in the underlying routes.
-    val httpApp = (
+    httpApp = (
       BotRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
         BotRoutes.dumpRequestRoutes[F](dumpReqAlg) <+>
         BotRoutes.botHandlerRoutes[F](botHandlerAlg)
     ).orNotFound
+    server <- EitherT.rightT(buildServer(httpApp))
+  } yield server
 
-    for {
-      _ <-
-        EmberServerBuilder
-          .default[F]
-          .withHost(ipv4"0.0.0.0")
-          .withPort(port"8080")
-          .withHttpApp(httpApp)
-          .build
-    } yield ()
-  }.useForever
+  private def buildServer[F[_]: Async](app: HttpApp[F]): Resource[F, Server] =
+    EmberServerBuilder
+      .default[F]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(app)
+      .build
 
-  private def loadEnv(name: String): String =
-    sys.env.get(name).getOrElse(throw new Exception(s"env-var $name is not present"))
+  private def loadEnvF[F[_]: Async: Env](name: String): EitherT[F, String, String] = for {
+    v <- EitherT.fromOptionF(Env[F].get(name), s"env-var $name is not present")
+  } yield v
 }
--- src/main/scala/h1rono/Main.scala
+++ src/main/scala/h1rono/Main.scala
@@ -4,5 +4,7 @@ import cats.effect.{IO, IOApp}
 import h1rono.BotServer
 
 object Main extends IOApp.Simple {
-  val run = BotServer.run[IO]
+  val run = for {
+    serve <- BotServer.run[IO].toOption.value
+  } yield serve.get.use(_ => IO.never)
 }
